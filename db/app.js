4d4a919b71b3124b4401a61a71bbc125047b78e9
Ahsan4k <83755681+Ahsan4k@users.noreply.github.com>
2021-08-27 12:32:24 +0500

    db

--- db/app.js
+++ db/app.js
@@ -1,6 +1,8 @@
 var MongoClient = require('mongodb').MongoClient;
 
-const url = "mongodb://localhost:27017/mydb";
+//const url = "mongodb://localhost:27017/mydb";
+
+const url = "mongodb+srv://ahsan123:ksingh1@cluster0.ihdsy.mongodb.net/myFirstDatabase"
 
  await MongoClient.connect(url, async(err,db) => {
     if(err,db) console.log(err);
